101c801a7b0fbcc38766b754ae4656e23d92b320
scx567888 <scx567888@outlook.com>
2025-04-17 14:57:06 +0800

    v0.1.4

--- scx-admin/_test/src/views/a1.vue
+++ scx-admin/_test/src/views/a1.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <easy-ckeditor v-model="text"/>
-        <easy-ckeditor v-model="text"/>    
+        <easy-ckeditor v-model="text"/>
     </div>
 </template>
 <script setup>
 import EasyCkeditor from "../../../components/easy-ckeditor/index.vue";
 import {ref} from "vue";
 
-const text=ref("一些文本");
+const text = ref("一些文本");
 
 </script>
 
