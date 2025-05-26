a7bec9b3a6ebc61a2ab6c10fd5832a06bcfdcbd1
scx567888 <scx567888@outlook.com>
2025-05-26 18:59:53 +0800

    dev

---
+++ scx-data/query/BuildControl.js
@@ -0,0 +1,45 @@
+class BuildControl {
+
+    #value;
+
+    constructor(value) {
+        this.#value = value;
+    }
+
+}
+
+const SKIP_IF_NULL = new BuildControl("SKIP_IF_NULL");
+const SKIP_IF_EMPTY_LIST = new BuildControl("SKIP_IF_EMPTY_LIST");
+const SKIP_IF_EMPTY_STRING = new BuildControl("SKIP_IF_EMPTY_STRING");
+const SKIP_IF_BLANK_STRING = new BuildControl("SKIP_IF_BLANK_STRING");
+const USE_EXPRESSION = new BuildControl("USE_EXPRESSION");
+const USE_EXPRESSION_VALUE = new BuildControl("USE_EXPRESSION_VALUE");
+
+
+function checkUseExpression(...controls) {
+    for (let control of controls) {
+        if (control === USE_EXPRESSION) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkUseExpressionValue(...controls) {
+    for (let control of controls) {
+        if (control === USE_EXPRESSION_VALUE) {
+            return true;
+        }
+    }
+    return false;
+}
+
+export {
+    BuildControl,
+    SKIP_IF_NULL,
+    SKIP_IF_EMPTY_LIST,
+    SKIP_IF_BLANK_STRING,
+    SKIP_IF_EMPTY_STRING,
+    checkUseExpression,
+    checkUseExpressionValue,
+};
